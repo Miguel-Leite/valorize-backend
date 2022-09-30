492b87cc19fad059368f6b6d6e41cdc9c7743aa5
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-01 00:08:45 +0100

    config database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import {AppDataSource} from './database/connection';
 import 'reflect-metadata';
 import './database';
 import express, { response } from 'express';
 
 const app = express();
 
+AppDataSource.initialize().then(()=> {
+    console.log("Banco de dados conectado")
+})
 
 app.listen(3000,()=> console.log("Server is run"));
