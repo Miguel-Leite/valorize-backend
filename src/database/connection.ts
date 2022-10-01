d8dd57a0ecc932d1ed18d89724b47f92a2207eb3
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-01 11:18:08 +0100

    editado arquivo gitignore e executando as migrations

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,4 +5,5 @@ export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "src/database/database.sqlite",
     migrations: ["src/database/migrations/*.ts"],
+    entities: ["src/entity"]
 });
