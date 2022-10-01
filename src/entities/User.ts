c427129e5b453d71031e2c428e0d8953d6b1c8b6
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-01 11:54:27 +0100

    Entity User e UserRepositories criado

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,8 +1,31 @@
-import { Entity } from "typeorm"
-
-@Entity()
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn  } from "typeorm"
+import { v4 as uui } from 'uuid'
+@Entity("users")
 class User {
 
+    @PrimaryColumn()
+    readonly id:string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor () {
+        if (!this.id) {
+            this.id = uui()
+        }
+    }
 }
 
 export { User }
