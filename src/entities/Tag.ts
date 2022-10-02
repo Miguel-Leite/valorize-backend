86233b4db0a442d1f47d0ee8ff932471c92161ec
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-02 17:09:51 +0100

    Entidade e repositorio da tag criadas

---
+++ src/entities/Tag.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm"
+import { v4 as uui } from 'uuid'
+
+@Entity()
+export class Tag {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+         
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uui()
+        }
+    }
+}
