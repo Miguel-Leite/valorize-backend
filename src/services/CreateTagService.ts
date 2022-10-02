062462be82c398475d3fae990ea1c42f0ad3ad83
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-02 17:44:38 +0100

    repositorio e serviço da tag criado com sucesso!

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,28 @@
+import { AppDataSource } from "../database/data-source";
+import { Tag } from "../entities/Tag";
+
+
+
+
+export class CreateTagServices {
+
+    async execute (name: string) {
+        const tagasRepositories = AppDataSource.getRepository(Tag);
+
+        if (!name) {
+            throw new Error("Incorrect name!");
+        }
+
+        const tagAlreadyExists = await tagasRepositories.findOneBy({name:name})
+
+        if (tagAlreadyExists) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagasRepositories.create({name: name});
+
+        await tagasRepositories.save(tag)
+
+        return tag;
+    }
+}
