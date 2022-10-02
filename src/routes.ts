97a73ea5fbb3128b8b6d677d2fbd4fae3518d4a0
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-02 17:47:33 +0100

    adicionando nova rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUsersControllers';
 
 const router = Router()
 
 const createUsersController = new CreateUserController(); 
+const createTagController = new CreateTagController()
 
 router.post('/users',createUsersController.handle)
+router.post('/tags',createTagController.handle)
 
 export { router }
