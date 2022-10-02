75ec604d83d04810fd5ed3e45611944807c353cb
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-02 17:10:25 +0100

    importando middleware para exibir erros de exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,30 @@
-import {AppDataSource} from './database/data-source';
 import 'reflect-metadata';
-import express, { response } from 'express';
+import {AppDataSource} from './database/data-source';
+import express, { Request, Response, NextFunction } from 'express';
+import "express-async-errors"
+
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json())
+
 app.use(router);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+        return response.status(400).json({
+            error: error.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 AppDataSource.initialize().then(()=> {
     console.log("Database connected!")
 })
 
-app.listen(3000,()=> console.log("Server is run"));
+app.listen(3000,()=> console.log("Server is running"));
