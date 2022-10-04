79fce92d60b0a02e9b4897650ebb89ead2940b94
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-04 12:19:07 +0100

    entidade compliment criado

---
+++ src/entities/Compliment.entity.ts
@@ -0,0 +1,51 @@
+import { 
+    Entity, 
+    PrimaryColumn, 
+    Column, 
+    CreateDateColumn, 
+    UpdateDateColumn,  
+    JoinColumn,
+    ManyToOne
+} from "typeorm";
+import { v4 as uui } from 'uuid';
+import { Tag } from "./Tag.entity";
+import { User } from "./User.entity";
+
+@Entity('compliments')
+export class Compliment {
+
+    @PrimaryColumn()
+    readonly id:string;
+
+    @Column()
+    user_sender: string;
+
+    @Column()
+    user_receiver: string;
+
+    @JoinColumn({name: "user_receiver"})
+    @ManyToOne(() => User)
+    userReceiver: User;
+
+    @Column()
+    tag_id: string;
+
+    @JoinColumn({name: "tag_id"})
+    @ManyToOne(() => Tag)
+    tag: Tag;
+
+    @Column()
+    message: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor () {
+        if (!this.id) {
+            this.id = uui();
+        }
+    }
+}
