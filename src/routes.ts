bd1085555c3dceb319729c28a3934054131d52ae
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-04 15:20:08 +0100

    adicionando novas routas

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,9 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUsersControllers';
+import { ListTagsController } from './controllers/ListTagsController';
+import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
+import { ListUserSendComplimentsController } from './controllers/ListUserSendComplimentsController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
@@ -12,11 +15,22 @@ const createUsersController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
+const listTagsController = new ListTagsController();
+
+
+router.get('/users', createUsersController.get);
+
 
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
-router.get('/users', createUsersController.get);
 router.post('/users', createUsersController.handle);
 router.post('/login', authenticateUserController.handle);
 router.post('/compliments', ensureAuthenticated, createComplimentController.handle);
+
+
+router.get('/users/compliments/send', ensureAuthenticated, listUserSendComplimentsController.handle);
+router.get('/users/compliments/receive', ensureAuthenticated, listUserReceiveComplimentsController.handle);
+router.get('/tags', listTagsController.handle);
 
 export { router }
