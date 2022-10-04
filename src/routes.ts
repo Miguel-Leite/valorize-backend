90c35fb0532f2717634f27ea6a83d8534bc90055
Miguel-Leite <miguelleite200leite@gmail.com>
2022-10-04 12:18:51 +0100

    adicionado novas routas

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,7 @@ const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
 
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle);
+router.get('/users', createUsersController.get);
 router.post('/users', createUsersController.handle);
 router.post('/login', authenticateUserController.handle);
 router.post('/compliments', createComplimentController.handle);
